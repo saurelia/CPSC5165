1fb571561e9ca5fdb8e209c0a24e29ccc6ae587f
saurelia <dobugazi1@gmail.com>
2015-01-30 14:03:58 -0500

    Added items 15 - 18

--- Ex04Cart/Cart.aspx.cs
+++ Ex04Cart/Cart.aspx.cs
@@ -7,5 +7,45 @@ using System.Web.UI.WebControls;
 
 public partial class Cart : System.Web.UI.Page
 {
-   
+    private CartItemList cart;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //retrieve cart object from session state on every postback
+        cart = CartItemList.GetCart();
+
+        //on initial page load, add cart items to list control
+        if(!IsPostBack)
+        {
+            this.DisplayCart();
+        }
+    }
+
+    private void DisplayCart()
+    {
+        //remove all current items from list control on the form
+        lstCart.Items.Clear();
+
+        //loop through cart and add each cart item's Display value to list control
+        for(int i=0; i< cart.Count; i++)
+        {
+            lstCart.Items.Add(this.cart[i].Display());
+
+        }
+    }
+    protected void btnRemove_Click(object sender, EventArgs e)
+    {
+        if(cart.Count >0)
+        {
+            if(lstCart.SelectedIndex > -1)
+            {
+                cart.RemoveAt(lstCart.SelectedIndex);
+                this.DisplayCart();
+            }
+            else
+            {
+                lblMessage.Text = "Please select item to remove";
+            }
+        }
+    }
 }
